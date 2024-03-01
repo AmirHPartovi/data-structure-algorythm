d886d38b32593de3f0e29ccbcf1762911ce46fa8
AmirHPartovi <a.partovi99@gmail.com>
2024-03-01 18:10:43 +0330

    change some things ...

---
+++ linkedList/index.js
@@ -0,0 +1,97 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+class LinkedList{
+    constructor(value){
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = this.head
+        this.length = 1
+    }
+    push(value){
+        const newNode = new Node(value)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }else{
+            this.tail.next = newNode
+            this.tail = newNode
+            this.length++
+            return this
+        }
+    }
+    pop(){
+        console.log('head',this.head)
+        if(! this.head)return undefined
+         
+        
+    
+        let temp = this.head
+        let pre = this.head
+        while(temp.next){
+            pre = temp
+            temp = temp.next
+        }
+        this.tail = pre
+        this.tail.next = null
+        this.length--
+        if(this.length===0){
+            this.head = null
+            this.tail = null
+        
+        }
+        return temp
+    }
+    unshift(value){
+        const newNode = new Node(value)
+        if(!this.head){
+            this.head = newNode
+            this.tail = this.head
+        }
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+        return this
+    }
+    shift(){
+        if(!this.head)return undefined
+        const temp = this.head
+        this.head = this.head.next
+        temp.next = null
+        this.length--
+        if(this.length===0){
+            this.tail=null
+        }
+        return temp
+    }
+    get(index){
+        if(index< 0 || index> this.length)return undefined
+        let temp = this.head
+        for(let i = 0 ; i < index ; i++){
+            temp = temp.next
+        }
+        return temp
+    }
+    set(index,value){
+        let temp = this.get(index)
+        if(temp){
+            temp.value = value
+            return true
+        }
+        return false
+        
+    }
+}
+
+let myLinkedList = new LinkedList(47)
+myLinkedList.push(12)
+myLinkedList.push(15)
+myLinkedList.push(18)
+myLinkedList.push(20)
+myLinkedList.push(40)
+
+myLinkedList.set(2,16)
+console.log(myLinkedList)
