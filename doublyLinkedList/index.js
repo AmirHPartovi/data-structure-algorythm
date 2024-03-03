5233d75b23549eafd831080813bebed70be96463
AmirHPartovi <a.partovi99@gmail.com>
2024-03-03 09:07:49 +0330

    doublyLinkedList shift & unshift method added

--- doublyLinkedList/index.js
+++ doublyLinkedList/index.js
@@ -29,23 +29,57 @@ class DoublyLinkedList{
         return this
     }
     pop(){
-        if(!this.head) return undefined
-        else{
+        if(!this.length===0) return undefined
+        let temp = this.tail
+        if(this.length === 1){
             let temp = this.tail
-            this.tail = temp.prev
+            this.head=null
+            this.tail=null
+        }
+        else{
+            this.tail = this.tail.prev
             temp.prev = null
             this.tail.next = null
-            this.length--
-            return temp
-
         }
+        this.length-- 
+        return temp 
     }
+    unshift(value){
+        let newNode = new Node(value)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }else{
+            newNode.next = this.head
+            this.head.prev = newNode
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+    shift(){
+        if(this.length===0) return undefined
+        let temp = this.head
+        if(this.length === 1){
+            this.head = null
+            this.tail = null
+        }
+        else{
+            this.head = temp.next
+            temp.next = null
+            this.head.prev =null
+        }
+        this.length--
+        return temp
+    }
+
 }
 
 
 const myDoublyLinkedList = new DoublyLinkedList(12)
 myDoublyLinkedList.push(15)
 myDoublyLinkedList.push(22)
-myDoublyLinkedList.pop()
-myDoublyLinkedList.pop()
+// myDoublyLinkedList.pop()
+// myDoublyLinkedList.unshift(4)
+// myDoublyLinkedList.shift()
 console.log(myDoublyLinkedList)
