b6176dc0f479ff7beace105b6fa73cc7397ba096
AmirHPartovi <a.partovi99@gmail.com>
2024-03-03 07:56:47 +0330

    Linked List Complete methods

--- linkedList/index.js
+++ linkedList/index.js
@@ -25,7 +25,7 @@ class LinkedList{
     }
     pop(){
         console.log('head',this.head)
-        if(! this.head)return undefined
+        if(!this.head)return undefined
          
         
     
@@ -84,6 +84,46 @@ class LinkedList{
         return false
         
     }
+    insert(index , value){
+        if( index <0 || index > this.length) return false
+        if( index === 0) return this.unshift(value)
+        if( index === this.length) return this.push(value)
+        let newNode = new Node(value)
+        let temp = this.get(index - 1)
+        newNode.next = temp.next
+        temp.next = newNode
+        this.length++
+        return true
+
+    }
+    remove(index){
+        if( index<0 || index> this.lenght) return undefined
+        if(index===0) return this.shift(index)
+        if(index === this.length) return this.pop(index)
+
+        let pre = this.get(index - 1)
+        let temp = pre.next
+        pre.next = temp.next
+        temp.next = null
+        this.length--
+        return temp
+    }
+    reverse(){
+        if(this.length === 0) return undefined
+        let temp =this.head
+        this.head = this.tail
+        this.tail =temp
+
+        let next = temp.next
+        let prev = null
+        for(let i = 0 ; i<this.length ; i++){
+            next = temp.next
+            temp.next = prev
+            prev = temp 
+            temp = next
+        }
+        return this
+    }
 }
 
 let myLinkedList = new LinkedList(47)
@@ -92,6 +132,9 @@ myLinkedList.push(15)
 myLinkedList.push(18)
 myLinkedList.push(20)
 myLinkedList.push(40)
-
+myLinkedList.set(1,4)
 myLinkedList.set(2,16)
+myLinkedList.remove(1)
+myLinkedList.insert(1,88)
+myLinkedList.reverse()
 console.log(myLinkedList)
