e9ed4d41d5ca2c0c2db2f3c638ea9fa4a030b7a5
AmirHPartovi <a.partovi99@gmail.com>
2024-03-04 07:30:21 +0330

    stack comment changes

--- stack/index.js
+++ stack/index.js
@@ -1,3 +1,7 @@
+//change script section folder name in package.json to run this ;)//
+//npm install ( to install nodemon and go live )
+//npm run watch
+
 class Node{
     constructor(value){
         this.value = value 
