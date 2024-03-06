fc7a3502734f71c1caa414ba655051e010f8457d
AmirHPartovi <a.partovi99@gmail.com>
2024-03-06 09:33:26 +0330

    binarySearchTree insert method added

--- binarySearchTree/index.js
+++ binarySearchTree/index.js
@@ -14,8 +14,41 @@ class BST{
     constructor(){
         this.root = null
     }
+    insert(value){
+        const newNode = new Node(value)
+        if(this.root === null)
+        {
+            this.root = newNode
+            return this 
+        }
+        let temp = this.root
+        while(true){
+            if(newNode.value === temp.value) return undefined
+            if(newNode.value < temp.value){
+                if(temp.left === null){
+                    temp.left = newNode
+                    return this
+                }else{
+                    temp = temp.left
+                }
+            }else{
+                if(temp.right === null){
+                    temp.right = newNode
+                    return this
+                }else{
+                    temp = temp.right
+                }
+            }
+        }
+    }
+
 }
 
 let myBST = new BST()
+myBST.insert(10)
+myBST.insert(11)
+myBST.insert(8)
+myBST.insert(9)
+myBST.insert(12)
 
 console.log(myBST)
