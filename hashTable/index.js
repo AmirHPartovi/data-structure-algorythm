fd2e35c05001f7f269da42bc0229e56e99e05587
AmirHPartovi <a.partovi99@gmail.com>
2024-03-16 04:10:21 +0330

    hashTable constructor & hash function

---
+++ hashTable/index.js
@@ -0,0 +1,16 @@
+class HashTable{
+    constructor( size = 7 ){
+        this.dataMap = new Array(size)
+    }
+    _hash(key){
+        let hash = 0
+        for(let i=0 ; i<key.length ; i++){
+            hash = (hash + hash.charCodeAt(i) * 23) %this.dataMap.length
+        }
+        return hash
+    }
+}
+
+const myHashTable = new HashTable()
+
+ 
