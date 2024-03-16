38b243227f5b1e6fec70aaf5c3b97eee6d7dd9ba
AmirHPartovi <a.partovi99@gmail.com>
2024-03-16 06:51:59 +0330

    graphs removeEdge

--- graphs/index.js
+++ graphs/index.js
@@ -21,14 +21,30 @@ class Graph{
 
         }
     }
+    removeEdge(vertex1 , vertex2){
+        if(this.adjacencyList[vertex1] && this.adjacencyList[vertex2]){
+
+            this.adjacencyList[vertex1] = this.adjacencyList[vertex1]
+            .filter( vertex => vertex !== vertex2)
+
+            this.adjacencyList[vertex2] = this.adjacencyList[vertex2]
+            .filter( vertex => vertex !== vertex1)
+            return true
+        }
+        return false
+    }
 }
 
 let myGraph = new Graph()
 
 myGraph.addVertex('a')
 myGraph.addVertex('b')
+myGraph.addVertex('d')
 myGraph.addVertex('a')
 myGraph.addEdge('a','b') 
+myGraph.addEdge('d','b') 
 myGraph.addEdge('c ','b') 
+myGraph.removeEdge('a','c')
+myGraph.removeEdge('a','b')
 
 console.log(`my Graph`,myGraph)
