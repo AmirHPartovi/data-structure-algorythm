7b9dc175210e5a4819e0b8d9fa39b26c8181ad91
AmirHPartovi <a.partovi99@gmail.com>
2024-03-19 08:00:40 +0330

    quick sort

--- sorts/index.js
+++ sorts/index.js
@@ -46,11 +46,11 @@ function insertionSort(array){
 //merge sort
 
 function merge(arr1 ,arr2){
-    let combined = []
+    const combined = []
     let i = 0
     let j = 0
     while( i < arr1.length && j < arr2.length){
-        if(arr1[i]  < arr2[j]){
+        if(arr1[i] < arr2[j]){
             combined.push(arr1[i])
             i++
         }else{
@@ -67,10 +67,13 @@ function merge(arr1 ,arr2){
     }
     
     return combined
+        
+
+
 } 
 function mergeSort(array){
     if(array.length === 1) return array
-    
+
     let mid = Math.floor(array.length/2)
     let left = array.slice(0,mid)
     let right = array.slice(mid)
@@ -79,10 +82,42 @@ function mergeSort(array){
 }
 
 
-let arr1=[1 , 6, 9, 3, 8 ,55 ,23 ,90 , 4 ,12 , 18 , 1 , 7 , 15]
+//Quick Sort
+
+function swap(array , firstIndex , secondIndex){
+    let temp = array[firstIndex]
+    array[firstIndex] = array[secondIndex]
+    array[secondIndex] = temp
+}
+
+function pivot(array , pivotIndex = 0 , endIndex = array.length-1){
+    let swapIndex = pivotIndex
+    for(let i = pivotIndex + 1 ;i < endIndex ; i++){
+        if(array[i] < array[pivotIndex]){
+            swap++;
+            swap(array , swapIndex , i); 
+        }
+    } 
+    swap(array , pivotIndex , swapIndex)
+    return swapIndex
+}
+
+function quickSort(array , left = 0 ,right = array.length-1){
+    if( left < right){
+        let pivotIndex = pivot(array,left,right)
+        quickSort(array,left , pivotIndex-1)
+        quickSort(array, pivotIndex +1, right)
+    }
+    return array
+}
+
+let arr1=[  , 6, 9, 3, 8 ,55 ,23 ,3 ,6 ,0 ,90 ,3 ,4 ,12 , 18 , 1 , 7 , 15]
 
 console.log(`bubble sort result : ` ,bubbleSort(arr1))
 console.log(`selection  sort result : ` ,selectionSort(arr1))
 console.log(`insertion  sort result : ` ,insertionSort(arr1))
 console.log(`merge function (not sorted ) result : ` ,merge(arr1,[5,8,2,1,0,9,0,3,7]))
+console.log(`merge function (not sorted ) result : ` ,merge([1,23,4,6],[1 ,2 ,3 ,4]))
 console.log(`merge sort result : ` ,mergeSort(arr1))
+console.log(`pivot function (not sorted) :` , pivot(arr1))
+console.log(`quick sort :` , quickSort(arr1))
